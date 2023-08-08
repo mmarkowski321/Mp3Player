0411be85c959bd62219cdae82a37bd45ba8986a1
mmarkowski321 <markowskimilosz989@gmail.com>
2023-08-08 14:52:40 +0200

    Start of project Mp3

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module Mp3Player {
+    requires javafx.graphics;
+
+    exports pl.javastart.example to javafx.graphics;
+}
---
+++ src/main/java/pl/javastart/example/Main.java
@@ -0,0 +1,16 @@
+package pl.javastart.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Mp3 player");
+        stage.show();
+    }
+}
